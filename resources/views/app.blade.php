3ab0ad68c43cd658fa82b60e892fd707fa0a765d
roufq <roufmawanto963@gmail.com>
2026-06-10 10:09:30 +0700

    feat: add base Blade layout template with Inertia and Vite configuration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="ba-02ZXagnCKrtGWmCl8rD9qIcpWmyBXDwHG_L6_aDo" />
 
         <title inertia>{{ config('app.name', 'VidFlow') }}</title>
 
